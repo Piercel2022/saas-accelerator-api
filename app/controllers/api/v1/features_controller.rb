be1072d67cf9d4884bec508b67ecab0405c42a28
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2024-12-31 02:16:22 +0100

    Add the features controller

--- app/controllers/api/v1/features_controller.rb
+++ app/controllers/api/v1/features_controller.rb
@@ -1,7 +1,58 @@
-class Api::V1::FeaturesController < ApplicationController
-  def index
-  end
-
-  def show
+module Api
+  module V1
+    class FeaturesController < BaseController
+      before_action :set_feature, only: [:show, :update, :destroy]
+      
+      def index
+        @features = Feature.all
+        render json: @features,
+               each_serializer: FeatureSerializer
+      end
+      
+      def show
+        render json: @feature,
+               serializer: FeatureSerializer
+      end
+      
+      def create
+        authorize! :create, Feature
+        @feature = Feature.new(feature_params)
+        
+        if @feature.save
+          render json: @feature,
+                 serializer: FeatureSerializer,
+                 status: :created
+        else
+          render_error @feature.errors.full_messages
+        end
+      end
+      
+      def update
+        authorize! :update, @feature
+        
+        if @feature.update(feature_params)
+          render json: @feature,
+                 serializer: FeatureSerializer
+        else
+          render_error @feature.errors.full_messages
+        end
+      end
+      
+      def destroy
+        authorize! :destroy, @feature
+        @feature.update(status: 'archived')
+        head :no_content
+      end
+      
+      private
+      
+      def set_feature
+        @feature = Feature.find(params[:id])
+      end
+      
+      def feature_params
+        params.require(:feature).permit(:name, :code, :description, :status)
+      end
+    end
   end
 end
