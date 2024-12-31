1bee2ae97e65fd07eb7e528dc814845ae4c54530
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2024-12-31 11:52:57 +0100

    Add the enhanced user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+   :confirmable,  :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   belongs_to :organization
   has_many :subscriptions
